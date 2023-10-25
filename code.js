655000550a3710019d393b70a8eb4802dd08335b
Samuel Gonzalez <sgonza21@uwyo.edu>
2023-10-24 23:22:43 -0600

    initial version created, problems after subdividing the list into an empty causes problems, auxiliary function needed

--- code.js
+++ code.js
@@ -1,3 +1,24 @@
 function divideAndConquerSum(a) {
-    return a;
+    if (a == []) {
+        return 0
+    }
+
+    if(a.length == 1) {
+        return a[0];
+    }
+
+    lengthThird = Math.ceil(a.length / 3);
+
+    beginning = a.slice(0, lengthThird);
+    console.log("this is the beginning " + beginning)
+
+    middle = a.slice(lengthThird, (lengthThird*2));
+    console.log(" this is the middle " + middle);
+
+    end = a.slice((lengthThird * 2));
+    console.log(" this is the end " + end);
+
+    return divideAndConquerSum(beginning) + divideAndConquerSum (middle) + divideAndConquerSum(end)
 }
+console.log(divideAndConquerSum([1,7,8]))
+
